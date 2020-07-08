e37554f7d97e2a87ee1c57d2393a247af233ccd0
Acer <bikashkatwalcr7@gmail.com>
2020-07-08 19:47:22 +0545

    Adding posts

--- app/src/main/java/com/example/photoblog/LoginActivity.java
+++ app/src/main/java/com/example/photoblog/LoginActivity.java
@@ -97,3 +97,4 @@ public class LoginActivity extends AppCompatActivity {
         finish();
     }
 }
+
--- app/src/main/java/com/example/photoblog/NewPostActivity.java
+++ app/src/main/java/com/example/photoblog/NewPostActivity.java
@@ -1,22 +1,172 @@
 package com.example.photoblog;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class NewPostActivity extends AppCompatActivity {
 
     private Toolbar newPostToolBar;
+    private ImageView newPostImage;
+    private EditText newPostDesc;
+    private Button newPostBtn;
+
+    private Uri postImageUri = null;
+
+    private ProgressDialog pd;
+
+
+    //Firebase
+    private StorageReference storageReference;
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth firebaseAuth;
+
+    private String current_user_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
 
+        //Firebase initializing
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        current_user_id = firebaseAuth.getCurrentUser().getUid();
+
+        pd = new ProgressDialog(this);
+
         newPostToolBar = findViewById(R.id.new_post_toolbar);
         setSupportActionBar(newPostToolBar);
         getSupportActionBar().setTitle("Add New Post");
+
+        newPostImage = findViewById(R.id.new_post_image);
+        newPostDesc = findViewById(R.id.new_post_desc);
+        newPostBtn = findViewById(R.id.post_btn);
+
+        newPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .setMinCropResultSize(512, 512)
+                        .setAspectRatio(1, 1)
+                        .start(NewPostActivity.this);
+            }
+        });
+
+
+        newPostBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final String desc = newPostDesc.getText().toString();
+
+                if (!TextUtils.isEmpty(desc) && postImageUri != null) {
+
+
+                    pd.setMessage("Uploading. . .");
+                    pd.show();
+
+                    String randomName = FieldValue.serverTimestamp().toString();
+
+                    StorageReference filePath = storageReference.child("post_images").child(randomName + ".jpg");
+                    filePath.putFile(postImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                            if (task.isSuccessful()) {
+                                task.getResult().getStorage().getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        Map<String, Object> postMap = new HashMap<>();
+                                        postMap.put("image_url", uri.toString());
+                                        postMap.put("desc", desc);
+                                        postMap.put("user_id", current_user_id);
+                                        postMap.put("timestamp", FieldValue.serverTimestamp());
+
+                                        firebaseFirestore.collection("Posts").add(postMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<DocumentReference> task) {
+                                                if (task.isSuccessful()) {
+                                                    pd.dismiss();
+                                                    Toast.makeText(NewPostActivity.this, "Post was added.", Toast.LENGTH_SHORT).show();
+                                                    Intent intent = new Intent(NewPostActivity.this, MainActivity.class);
+                                                    startActivity(intent);
+                                                    finish();
+                                                } else {
+                                                    pd.dismiss();
+                                                    String error = task.getException().getMessage();
+                                                    Toast.makeText(NewPostActivity.this, "ERROR:: " + error, Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+
+                                    }
+                                });
+                            } else {
+                                pd.dismiss();
+                                String err = task.getException().getMessage();
+                                Toast.makeText(NewPostActivity.this, "Error: " + err, Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+
+                postImageUri = result.getUri();
+                newPostImage.setImageURI(postImageUri);
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+
+                Exception error = result.getError();
+
+            }
+        }
+    }
+
 }
