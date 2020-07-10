48666fb11786079d7adfa9a936dcb2e4f67b0ea4
Acer <bikashkatwalcr7@gmail.com>
2020-07-10 15:09:27 +0545

    Showing post description in home page

---
+++ app/src/main/java/com/example/photoblog/Model/BlogPost.java
@@ -0,0 +1,39 @@
+package com.example.photoblog.Model;
+
+
+public class BlogPost {
+    public String user_id, image_url, desc;
+
+    public BlogPost() {
+    }
+
+    public BlogPost(String user_id, String image_url, String desc) {
+        this.user_id = user_id;
+        this.image_url = image_url;
+        this.desc = desc;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
